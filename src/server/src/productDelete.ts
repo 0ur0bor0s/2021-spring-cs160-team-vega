c17fbe3d45ec583e9a90705ec030c803f0955af9
kerrykov <kovacevickerry@gmail.com>
2021-04-24 18:07:38 -0700

    updated delete

--- src/server/src/productDelete.ts
+++ src/server/src/productDelete.ts
@@ -1,12 +1,28 @@
 import { getConnection, ObjectID } from "typeorm";
 import { Product } from "./entity/mongodb/Product";
+import { Arg, Mutation } from 'type-graphql'
 
 //Deletes Product with given id from mongodb database
-export const deleteListing = async (product_id: ObjectID) => {
-    try {
-        await getConnection("productDBConnection").getMongoRepository(Product).delete({ _id: product_id });
-        console.log("Deleted product with id: " + product_id);
-    } catch (error) {
-        console.log("Unable to delete product with id: " + product_id);
+export class productDelete {
+    @Mutation(() => Boolean)
+    async deleteListing(@Arg('product_id') product_id: ObjectID) {
+        try {
+            await getConnection("productDBConnection").getMongoRepository(Product).delete({ _id: product_id });
+            console.log("Deleted product with id: " + product_id);
+        } catch (err) {
+            console.log("Unable to delete product: " + err);
+            return false;
+        }
+
+        return true;
     }
 }
+
+//export const deleteListing = async (product_id: ObjectID) => {
+//    try {
+//        await getConnection("productDBConnection").getMongoRepository(Product).delete({ _id: product_id });
+//        console.log("Deleted product with id: " + product_id);
+//    } catch (error) {
+//        console.log("Unable to delete product with id: " + product_id);
+//    }
+//}
